f84130ae92dd3a8af4742711424b63f815033b80
capthiron <24925821+capthiron@users.noreply.github.com>
2022-03-29 17:48:48 +0200

    refactor: extend golang lsp config

--- lua/capthiron/lsp.lua
+++ lua/capthiron/lsp.lua
@@ -24,11 +24,29 @@ local on_attach = function(_, bufnr)
     buf_set_keymap('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
 end
 
+local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities()) -- autocompletion integration with cmp
 for _, lsp in ipairs(servers) do
-    local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities()) -- autocompletion integration with cmp
-	nvim_lsp[lsp].setup {
-		on_attach = on_attach,
-		capabilities = capabilities
-	}
+	if nvim_lsp ~= 'gopls' then
+		nvim_lsp[lsp].setup {
+			on_attach = on_attach,
+			capabilities = capabilities
+		}
+	end
 end
 
+-- golang specific setup
+nvim_lsp.gopls.setup{
+	cmd = {'gopls'},
+	capabilities = capabilities,
+	    settings = {
+			gopls = {
+				experimentalPostfixCompletions = true,
+				analyses = {
+					unusedparams = true,
+					shadow = true,
+				},
+				staticcheck = true,
+		    },
+	    },
+	on_attach = on_attach,
+}
