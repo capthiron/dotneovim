a293d144effd775406f29019ed226a727393c118
capthiron <24925821+capthiron@users.noreply.github.com>
2022-03-29 14:25:28 +0200

    feat: add treesitter and autocompletion via nvim-cmp with lsp integration

--- lua/capthiron/lsp.lua
+++ lua/capthiron/lsp.lua
@@ -25,7 +25,10 @@ local on_attach = function(_, bufnr)
 end
 
 for _, lsp in ipairs(servers) do
+    local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities()) -- autocompletion integration with cmp
 	nvim_lsp[lsp].setup {
-		on_attach = on_attach
+		on_attach = on_attach,
+		capabilities = capabilities
 	}
 end
+
