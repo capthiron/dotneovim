6ff67055f99a225e3f5cf8a46c855c1c65ebb311
capthiron <24925821+capthiron@users.noreply.github.com>
2022-04-09 00:02:11 +0200

    feat: enable automatic formatting of elm files on save

--- lua/capthiron/lsp.lua
+++ lua/capthiron/lsp.lua
@@ -22,6 +22,7 @@ local on_attach = function(_, bufnr)
     buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
     buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
     buf_set_keymap('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+    buf_set_keymap('n', '<C-f>', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
 end
 
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities()) -- autocompletion integration with cmp
