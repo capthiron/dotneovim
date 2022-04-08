cecd709abff090f4b5d1ad9326d2236cdfdfc550
capthiron <bananapies97@googlemail.com>
2022-04-09 00:02:11 +0200

    feat: enable automatic formatting of elm files on save

--- lua/capthiron/autosave.lua
+++ lua/capthiron/autosave.lua
@@ -1,1 +1,10 @@
-require("autosave").setup()
+local autosave = require("autosave")
+
+autosave.hook_after_saving = function ()
+	local file = '%F'
+	if file:sub(-4) == ".elm" then
+		vim.lsp.buf.formatting()
+	end
+end
+
+autosave.setup()
