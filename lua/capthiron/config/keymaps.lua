8fbbf1c235133bb230257d2a545b29e48013fb3d
capthiron <24925821+capthiron@users.noreply.github.com>
2023-09-03 17:52:48 +0200

    chore: update keymaps.lua

--- lua/capthiron/config/keymaps.lua
+++ lua/capthiron/config/keymaps.lua
@@ -25,11 +25,13 @@ keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 -- copy to clipboard
 keymap.set({ "n", "v" }, "<leader>y", '"+Y')
+-- don't copy single character to clipboard
+keymap.set("n", "x", '"_x')
 
 -- window management
 keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
 keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
-keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
+keymap.set("n", "<leader>se", "<C-w>x", { desc = "Make splits equal size" }) -- make split windows equal width & height
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
 
 keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
@@ -39,5 +41,5 @@ keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) 
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
 
 -- increment/decrement numbers
-keymap.set("n", "<leader>k", "<C-a>", { desc = "Increment number" }) -- increment
-keymap.set("n", "<leader>j", "<C-x>", { desc = "Decrement number" }) -- decrement
+keymap.set("n", "<leader>=", "<C-a>", { desc = "Increment number" }) -- increment
+keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
