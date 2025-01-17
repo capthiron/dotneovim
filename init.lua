3e18138972f29d44f15f5daffaf06f55232ad113
capthiron <24925821+capthiron@users.noreply.github.com>
2025-01-17 10:14:10 +0100

    chore: Ignore .git/, node_modules/ and vendor/ folders in telescope

--- init.lua
+++ init.lua
@@ -393,11 +393,16 @@ require('lazy').setup({
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
         --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
+        defaults = {
+          file_ignore_patterns = {
+            '.git/',
+            'node_modules/',
+            'vendor/',
+          },
+          --   mappings = {
+          --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
+          --   },
+        },
         pickers = {
           find_files = {
             hidden = true,
