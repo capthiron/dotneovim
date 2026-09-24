bf1c1defb886759e735748f3103cad6f922419c7
capthiron <24925821+capthiron@users.noreply.github.com>
2026-09-24 15:24:36 +0200

    feat(transparent): persist state and add toggle keymap

--- lua/custom/plugins/transparent.lua
+++ lua/custom/plugins/transparent.lua
@@ -1,1 +1,18 @@
-return { 'xiyaowong/transparent.nvim' }
+return {
+  'xiyaowong/transparent.nvim',
+  -- Must load at startup so the persisted on/off state is applied on VimEnter.
+  lazy = false,
+  opts = {
+    -- clear_prefix('NeoTree') would clear these even while transparency is off.
+    extra_groups = {
+      'NeoTreeNormal',
+      'NeoTreeNormalNC',
+      'NeoTreeEndOfBuffer',
+      'RenderMarkdownCode',
+      'RenderMarkdownCodeBorder',
+    },
+  },
+  keys = {
+    { '<leader>tt', '<cmd>TransparentToggle<CR>', desc = '[T]oggle [t]ransparent background' },
+  },
+}
